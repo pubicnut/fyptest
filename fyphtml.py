c3b06907a6f9efdbfb77b0aafdef8d5466faae4f
pubicnut <ericyeap_1997@hotmail.com>
2021-12-31 11:47:24 +0800

    Update fyphtml.py

--- fyphtml.py
+++ fyphtml.py
@@ -1,14 +1,12 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+# Defining the home page of our site
 @app.route("/")
 def home():
-	return "Hello! This is the home page <h1>HELLO</h1>"
-
-@app.route("/<name>")
-def user(name):
-	return f"Hello {name}!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
