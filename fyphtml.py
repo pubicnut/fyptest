bdf9a9f92218670dbcd05d0acc8ca37403e0b08b
pubicnut <ericyeap_1997@hotmail.com>
2021-12-31 12:00:17 +0800

    Update fyphtml.py

--- fyphtml.py
+++ fyphtml.py
@@ -1,12 +1,41 @@
-from flask import Flask, render_template
+import streamlit as st
+import streamlit.components.v1 as components
 
-app = Flask(__name__)
-
-
-# Defining the home page of our site
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-if __name__ == "__main__":
-    app.run()
+components.html(
+"""
+<!-- TradingView Widget BEGIN -->
+<div class="tradingview-widget-container">
+  <div id="tradingview_80348"></div>
+  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/NASDAQ-AAPL/" rel="noopener" target="_blank"><span class="blue-text">AAPL Chart</span></a> by TradingView</div>
+  <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+  <script type="text/javascript">
+  new TradingView.widget(
+  {
+  "width": 980,
+  "height": 610,
+  "symbol": "NASDAQ:AAPL",
+  "timezone": "Etc/UTC",
+  "theme": "light",
+  "style": "1",
+  "locale": "en",
+  "toolbar_bg": "#f1f3f6",
+  "enable_publishing": true,
+  "withdateranges": true,
+  "range": "YTD",
+  "hide_side_toolbar": false,
+  "allow_symbol_change": true,
+  "details": true,
+  "hotlist": true,
+  "calendar": true,
+  "show_popup_button": true,
+  "popup_width": "1000",
+  "popup_height": "650",
+  "container_id": "tradingview_80348"
+}
+  );
+  </script>
+</div>
+<!-- TradingView Widget END -->
+""",
+    height=1080, width=1920
+)
